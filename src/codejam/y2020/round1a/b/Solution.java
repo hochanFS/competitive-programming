11c5aaf536e514415c5a8a4e89f5e7859a2c0c64
Hochan Lee <hochan.lee@financescript.com>
2020-05-11 04:18:56 -0400

    Solved codejam y2020 round1a/b

--- src/codejam/y2020/round1a/b/Solution.java
+++ src/codejam/y2020/round1a/b/Solution.java
@@ -8,22 +8,22 @@ import java.util.*;
 import java.util.StringTokenizer;
 import java.io.PrintWriter;
 
-public class Solution {
+/**
+ * Uses the property of Pascal where sum of row == 2^(row - 1)
+ */
 
-    public static final int[][] directions = {{0, 1}, {1, 1}, {1, 0}, {-1, 0}, {0, -1}, {-1, -1}};
+public class Solution {
 
     public static void main(String[] args) {
 
         In in = new In(System.in);
         PrintWriter out = new PrintWriter(System.out);
         try {
-            long[][] memo = new long[300][300];
-            memo[1][1] = 1;
             int T = in.nextInt();
             for (int i = 1; i <= T; i++) {
                 int N = in.nextInt();
                 out.printf("Case #%d:\n", i);
-                for (long[] values : solvePascal(N, memo)) {
+                for (int[] values : solvePascal(N)) {
                     out.printf("%d %d\n", values[0], values[1]);
                 }
             }
@@ -42,62 +42,37 @@ public class Solution {
         return output;
     }
 
-    private static List<long[]> solvePascal(int n, long[][] memo) {
-        String s = Integer.toBinaryString(n); // e.g. 6 -> 1 1 0
-        List<Long> paths = new ArrayList<>();
-        Set<Long> seen = new HashSet<>();
-        seen.add(302L);
-        dfs(302, seen, paths, memo, 0, n); // start with (1, 1) with empty paths...
-        return unwrapPath(paths);
-    }
-
-    private static boolean dfs(long current, Set<Long> visited, List<Long> paths, long[][] memo, long sum, long target) {
-        long n = current / 301;
-        long k = current % 301;
-        long val = sum + getPascalValue((int)n, (int)k, memo);
-        paths.add(current);
-        if (val > target)
-            return false;
-        if (val == target)
-            return true;
-        PriorityQueue<long[]> pq = new PriorityQueue<>((u, v) -> Long.compare(v[1], u[1]));
-        for (int[] direction : directions) {
-            long nextN = n + direction[0];
-            long nextK = k + direction[1];
-            long key = getKey(nextN, nextK);
-            if (nextK <= nextN && nextK >= 1 && !visited.contains(key)) {
-                long addition = getPascalValue((int)nextN, (int)nextK, memo);
-                if (addition + sum <= target) {
-                    pq.add(new long[]{key, addition + sum});
+    private static List<int[]> solvePascal(int n) {
+        List<int[]> output = new ArrayList<>();
+        if (n <= 30) {
+            for (int i = 1; i <= n; i++)
+            output.add(new int[]{i, 1});
+            return output;
+        }
+        n -= 30;
+        boolean left = true;
+        int countBit = 0;
+        for (int i = 0; i < 30; i++) {
+            output.add(new int[]{i + 1, left? 1: i + 1});
+            if (((n >> i) & 1) == 1) {
+                if (left) {
+                    for (int j = 1; j <= i; ++j) {
+                        output.add(new int[]{i + 1, j + 1});
+                    }
+                } else {
+                    for (int j = i - 1; j >= 0; j--) {
+                        output.add(new int[]{i + 1, j + 1});
+                    }
                 }
-
+                left = !left;
+                countBit++;
             }
-
         }
-        while(!pq.isEmpty()) {
-            long[] values = pq.poll();
-            long key = values[0];
-            long cum = values[1];
-            visited.add(key);
-            if (dfs(key, visited, paths, memo, cum, target))
-                return true;
-            visited.remove(key);
-            paths.remove(paths.size() - 1);
+        for (int i = 1; i <= countBit; i++) {
+            output.add(new int[]{30 + i, left? 1: 30 + i});
         }
-        return false;
-    }
+        return output;
 
-    private static long getPascalValue(int n, int k, long[][] memo) {
-        if (k == 1 || k == n)
-            return 1;
-        if (memo[n][k] != 0L)
-            return memo[n][k];
-        memo[n][k] = getPascalValue(n - 1, k - 1, memo) + getPascalValue(n - 1, k, memo);
-        return memo[n][k];
-    }
-
-    private static long getKey(long n, long k) {
-        return n * 301 + k;
     }
 
     //@
